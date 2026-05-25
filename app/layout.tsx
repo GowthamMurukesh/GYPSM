8aeb06fc8c00650fbff5a2407abef8eb6157d296
Gowtham M <135612455+GowthamMurukesh@users.noreply.github.com>
2026-05-25 10:39:35 +0530

    logoupdated

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,19 +14,20 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/royal gypsum .png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/royal gypsum .png',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/royal gypsum .png',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/royal gypsum .png',
+    // D:\projectsece\github\GYPSM\public\royal gypsum .png
   },
 }
 
